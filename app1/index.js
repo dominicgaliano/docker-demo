6ca53ffb91f90558897f81a293b07159c514ca39
Dominic Galiano <dgaliano@utexas.edu>
2023-11-03 14:59:44 -0400

    initial commit

---
+++ app1/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const morgan = require("morgan");
+const path = require("path");
+
+app.use(morgan("combined"));
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// demo, adding route to test out gh push action
+app.get("/api", (req, res) => {
+  res.status(200);
+  res.send('API route');
+});
+
+app.get("/api/test", (req, res) => {
+  res.status(200);
+  res.send('test route');
+});
+
+module.exports = app;
+
+if (require.main === module) {
+  // This code will only run if the file is being executed directly
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
